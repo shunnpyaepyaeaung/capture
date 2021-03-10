61b31a538253f37de9c2adbe9dd9343e85b2cc37
Shunn Pyae Pyae Aung <shunnpyaepyaeaung@Shunns-MacBook-Pro.local>
2021-03-10 16:05:58 +0630

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AboutUs from "./pages/AboutUs";
 import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
 
 const App = () => {
   return (
     <div>
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
