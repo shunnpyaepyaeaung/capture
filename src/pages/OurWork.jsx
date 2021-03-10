11cc230457eac45b74cbd966171577b25ec72bde
Shunn Pyae Pyae Aung <shunnpyaepyaeaung@Shunns-MacBook-Pro.local>
2021-03-10 19:12:12 +0630

    Our work page

--- src/pages/OurWork.jsx
+++ src/pages/OurWork.jsx
@@ -1,11 +1,59 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import athlete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
 
 const OurWork = () => {
   return (
-    <div>
-      <h1>Ourwork</h1>
-    </div>
+    <Work>
+      <Movie>
+        <h2>The Atlete</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={athlete} alt="Athlete Photo" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>The Racer</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={theracer} alt="Racer Photo" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>Good Times</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={goodtimes} alt="Goodtimes Photo" />
+        </Link>
+      </Movie>
+    </Work>
   );
 };
+
+const Work = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 5rem 10rem;
+  h2 {
+    padding: 1rem 0rem;
+  }
+`;
+
+const Movie = styled.div`
+  padding-bottom: 5rem;
+  .line {
+    height: 0.3rem;
+    background: #cccccc;
+    margin-bottom: 3rem;
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
 
 export default OurWork;
